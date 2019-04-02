5ce6e584da6f6277df8ea21bd224b3860ee9ab63
Mauricio Leal <mauriciolealmx@gmail.com>
2019-04-02 16:22:27 -0600

    Added mongo URI and urlParser to avoid warning.

--- server/server.js
+++ server/server.js
@@ -12,14 +12,16 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+// Connect String for Node.js 2.2.12
+const MONGO_URI = 'mongodb://emdbAdmin:32j1xTWIjgIg1q0a@email-marketing-shard-00-00-kcs7f.mongodb.net:27017,email-marketing-shard-00-01-kcs7f.mongodb.net:27017,email-marketing-shard-00-02-kcs7f.mongodb.net:27017/graphql-auth?ssl=true&replicaSet=email-marketing-shard-0&authSource=admin&retryWrites=true';
+
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
 
 // Connect to the mongoDB instance and log a message
 // on success or failure
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, { useNewUrlParser: true });
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
