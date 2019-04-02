ef3f3f08a6b65d68af9e975d46c7821d1f22a582
Mauricio Leal <mauriciolealmx@gmail.com>
2019-04-02 17:30:15 -0600

    ApolloProvider and client/store.

--- client/index.js
+++ client/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  // Uniquely identify every record that we fetch.
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <div>Auth Starter</div>
+    </ApolloProvider>
   );
 };
 
--- server/schema/types/user-type.js
+++ server/schema/types/user-type.js
@@ -1,10 +1,11 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
 
 const UserType = new GraphQLObjectType({
   // GraphQL requires a name for ObjectType.
   name: 'UserType',
   fields: {
+    id: { type: GraphQLID },
     email: { type: GraphQLString }
   }
 });
